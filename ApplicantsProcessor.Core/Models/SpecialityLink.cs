a0e0a69244df53a3819771de8867dd416d6bb322
Ilia Bylyba <sksf.bylyba@gmail.com>
2022-07-22 21:51:41 +0700

    add models

---
+++ ApplicantsProcessor.Core/Models/SpecialityLink.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicantsProcessor.Core.Models
+{
+    public class SpecialityLink
+    {
+        public string Link { get; set; }
+        public string Name { get; set; }
+        public string University { get; set; }
+    }
+}
